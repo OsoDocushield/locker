d220c9f284e1d4ff17ee6116fa3865ab89f37f5d
Yasha <jacobyidev@gmail.com>
2022-09-16 05:32:52 -0700

    feat: Use mongoose to connnect mongodb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,8 @@
 import { useSession } from "next-auth/react"
-import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 
 import Layout from "../components/layout"
-import clientPromise from "../lib/mongodb"
 
-export async function getServerSideProps(context: {}) {
-  try {
-    const client = await clientPromise
-    const db = client.db("users")
-
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
-  }
-}
-
-export default function Page({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Page() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
 
@@ -41,16 +21,7 @@ export default function Page({
   return (
     <Layout>
       <h1>NFT Locker, {session.user?.name}</h1>
-      <button>+ Locker</button>
-
-      {isConnected ? (
-        <h2 className="subtitle">You are connected to MongoDB</h2>
-      ) : (
-        <h2 className="subtitle">
-          You are NOT connected to MongoDB. Check the <code>README.md</code> for
-          instructions.
-        </h2>
-      )}
+      <button onClick={handleCreateLocker}>+ Locker</button>
     </Layout>
   )
 }
