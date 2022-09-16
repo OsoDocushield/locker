6475599479a76be5b47f8ee50c2b8dddceecb015
Yasha <jacobyidev@gmail.com>
2022-09-15 20:03:22 -0700

    chore: Add mongodb and connect it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
 import { useSession } from "next-auth/react"
-import Layout from "../components/layout"
+import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 
-export default function Page() {
+import Layout from "../components/layout"
+import clientPromise from "../lib/mongodb"
+
+export async function getServerSideProps(context: {}) {
+  try {
+    const client = await clientPromise
+    const db = client.db("users")
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+export default function Page({
+  isConnected,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { data: session, status } = useSession()
   const loading = status === "loading"
 
@@ -21,6 +42,15 @@ export default function Page() {
     <Layout>
       <h1>NFT Locker, {session.user?.name}</h1>
       <button>+ Locker</button>
+
+      {isConnected ? (
+        <h2 className="subtitle">You are connected to MongoDB</h2>
+      ) : (
+        <h2 className="subtitle">
+          You are NOT connected to MongoDB. Check the <code>README.md</code> for
+          instructions.
+        </h2>
+      )}
     </Layout>
   )
 }
