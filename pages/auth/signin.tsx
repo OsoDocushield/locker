f60a67b1c1289b5b095cc21a739d9caf339ec7d5
Yasha <jacobyidev@gmail.com>
2022-09-16 11:31:44 -0700

    feat: Add custom signin page

---
+++ pages/auth/signin.tsx
@@ -0,0 +1,26 @@
+import { getProviders, signIn } from "next-auth/react"
+import { InferGetServerSidePropsType } from "next"
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: { providers },
+  }
+}
+
+export default function SignIn({
+  providers,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log("APOLLO: 1057", providers)
+  return (
+    <>
+      {providers && Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  )
+}
